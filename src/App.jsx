2df7e9fc9fa130af1f604fba5666a0816d391126
pachipalaaravindyadav31-droid <pachipalaaravindyadav31@gmail.com>
2026-01-31 09:01:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "80px", fontFamily: "Arial" }}>
+      <h1>Hello World</h1>
+      <p>Welcome to React + Vite 🚀</p>
+    </div>
+  );
+}
+
+
+export default App
